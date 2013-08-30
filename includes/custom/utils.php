0de80d3b76fdd898fb3ba789d1ce24b07e1c59d5
sdockray <sean.patrick.dockray@gmail.com>
2013-08-30 14:34:36 +1000

    url sanitize

--- includes/custom/utils.php
+++ includes/custom/utils.php
@@ -490,6 +490,22 @@ function aaaart_utils_format_date($time, $how='l') {
 
 
 /*
+*/
+function aaaart_utils_sanitize($string, $force_lowercase = true, $anal = false) {
+    $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+                   "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
+                   "â€”", "â€“", ",", "<", ".", ">", "/", "?");
+    $clean = trim(str_replace($strip, "", strip_tags($string)));
+    $clean = preg_replace('/\s+/', "-", $clean);
+    $clean = ($anal) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    return ($force_lowercase) ?
+        (function_exists('mb_strtolower')) ?
+            mb_strtolower($clean, 'UTF-8') :
+            strtolower($clean) :
+        $clean;
+}
+
+/*
  *
  */
 function aaaart_truncate($str, $limit) {
