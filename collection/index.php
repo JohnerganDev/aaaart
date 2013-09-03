f01ec07760eb825d27ed874d997cbde5dd1f1020
sdockray <sean.patrick.dockray@gmail.com>
2013-09-03 23:47:36 +1000

    add to collection bug

--- collection/index.php
+++ collection/index.php
@@ -125,7 +125,7 @@ switch (aaaart_utils_get_server_var('REQUEST_METHOD')) {
 					}
 				break;
 				case 'add':
-					if (!empty($_POST['document_id']) && !empty($_POST['collection_id']) && aaaart_collection_check_perm('add', $_POST['collection_id'])) {
+					if (!empty($_POST['document_id']) && !empty($_POST['collection_id'])) {
             if (is_array($_POST['collection_id'])) {
               aaaart_collection_add_document_to_collections($_POST['collection_id'], $_POST['document_id'], true);
             } else {
